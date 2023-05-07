4f95531fc9cafb4c51828c6be92a44eb0797b7b1
guzhongren <guzhongren@live.cn>
2023-05-07 10:31:02 +0800

    test(test): remove semicolon

--- __tests__/lib/string-utils.spec.ts
+++ __tests__/lib/string-utils.spec.ts
@@ -1,5 +1,37 @@
+import { parseStringToJson } from '../../src/lib/string-utils'
+
 describe('parseStringToJson', () => {
   it('should work', () => {
     expect(6 * 7).toEqual(42)
+  })
+
+  const comment = '// comment here'
+  const chartOptionJSONString = `{
+    ${comment}
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+      }
+    ]
+  };`
+
+  it('should remove comment', () => {
+    const result = parseStringToJson(chartOptionJSONString)
+
+    expect(result.toString()).not.toContain(comment)
+  })
+
+  it('should remove semicolon', () => {
+    const result = parseStringToJson(chartOptionJSONString)
+
+    expect(result.toString()).not.toContain(';')
   })
 })
--- jest.config.ts
+++ jest.config.ts
@@ -101,6 +101,10 @@ export default {
 
   // A preset that is used as a base for Jest's configuration
   // preset: undefined,
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest'
+  },
 
   // Run tests from one or more projects
   // projects: undefined,
