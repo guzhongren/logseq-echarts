ab1b6ec36c3330c08b3461ed0b7c400f69d476d2
guzhongren <guzhongren@live.cn>
2023-05-07 10:49:32 +0800

    test(test): test dedent

--- __tests__/lib/string-utils.spec.ts
+++ __tests__/lib/string-utils.spec.ts
@@ -1,37 +1,67 @@
-import { parseStringToJson } from '../../src/lib/string-utils'
+import { dedent, parseStringToJson } from '../../src/lib/string-utils'
 
-describe('parseStringToJson', () => {
-  it('should work', () => {
-    expect(6 * 7).toEqual(42)
-  })
+describe('string-utils', () => {
 
   const comment = '// comment here'
-  const chartOptionJSONString = `{
-    ${comment}
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line'
-      }
-    ]
-  };`
+  const whiteSpaceAtStart = `
 
-  it('should remove comment', () => {
-    const result = parseStringToJson(chartOptionJSONString)
+  `
+    const chartOptionJSONString = `{
+      ${comment}\n
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+    };`
 
-    expect(result.toString()).not.toContain(comment)
+    const chartOptionsStringWithWhiteSpace = `${whiteSpaceAtStart}${chartOptionJSONString}`
+
+
+  describe('parseStringToJson', () => {
+    it('should work', () => {
+      expect(6 * 7).toEqual(42)
+    })
+
+    it('should remove comment', () => {
+      const result = parseStringToJson(chartOptionJSONString)
+
+      expect(result.toString()).not.toContain(comment)
+    })
+
+    it('should remove semicolon', () => {
+      const result = parseStringToJson(chartOptionJSONString)
+
+      expect(result.toString()).not.toContain(';')
+    })
   })
 
-  it('should remove semicolon', () => {
-    const result = parseStringToJson(chartOptionJSONString)
+  describe('dedent', ()=> {
+    it('should remove new line', () => {
+      const result = dedent(chartOptionJSONString)
 
-    expect(result.toString()).not.toContain(';')
+      expect(result).not.toContain('\\n')
+    })
+
+    it('should remove white space at start', () => {
+      const result = dedent(chartOptionsStringWithWhiteSpace)
+
+      expect(result.length).toEqual(308)
+    })
+
+    it('should not process string', () => {
+      const result = dedent(chartOptionJSONString)
+
+      expect(result.length).toEqual(311)
+    })
   })
+
 })
